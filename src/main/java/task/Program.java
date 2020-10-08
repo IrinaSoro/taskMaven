9b037d70da17d7cbf365fd9745cb50c7fc0a2d49
IrinaSoro <irinka-soro@mail.ru>
2020-10-08 10:39:13 +0300

    test add

--- src/main/java/task/Program.java
+++ src/main/java/task/Program.java
@@ -3,7 +3,7 @@ package task;
 public class Program {
     public static void main(String[] args) {
         AttachmentsGenerator gen = new AttachmentsGenerator();
-        gen.generate(7);
+        gen.generate(1);
 //        for (Attachment a : gen.generate()) {
 //            System.out.println(a.toString());
 //
--- src/test/java/task/AttachmentsGeneratorTest.java
+++ src/test/java/task/AttachmentsGeneratorTest.java
@@ -7,10 +7,11 @@ import java.io.IOException;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AttachmentsGeneratorTest {
     @Test
-    void name1() throws IOException {
+    void negativeNum() throws IOException {
         AttachmentsGenerator generator = new AttachmentsGenerator();
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
             generator.generate(-1);
@@ -19,7 +20,7 @@ class AttachmentsGeneratorTest {
     }
 
     @Test
-    void name() {
+    void number() {
         AttachmentsGenerator generator = new AttachmentsGenerator();
         int atNumber = 5;
         List<Attachment> res = generator.generate(atNumber);
@@ -27,4 +28,25 @@ class AttachmentsGeneratorTest {
         assertEquals(5, res.size());
     }
 
+    @Test
+    void instance() {
+        AttachmentsGenerator generator = new AttachmentsGenerator();
+        int atNumber = 1;
+        List<Attachment> res = generator.generate(atNumber);
+        //проверка результата
+        assertTrue(res.get(0) instanceof Document);
+    }
+
+    @Test
+    void instanceOf() {
+        AttachmentsGenerator generator = new AttachmentsGenerator();
+        int atNumber = 5;
+        List<Attachment> res = generator.generate(atNumber);
+        //проверка результата
+        assertTrue(res.get(0) instanceof Document);
+        assertTrue(res.get(1) instanceof Picture);
+        assertTrue(res.get(2) instanceof SoundFile);
+        assertTrue(res.get(3) instanceof Document);
+        assertTrue(res.get(4) instanceof Picture);
+    }
 }
